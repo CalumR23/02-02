08db9e2d5257faae80d64b4974bb8a1e321441a0
Calum <searagan@gmail.com>
2022-02-15 11:34:50 -0800

    iteration logic

---
+++ iteration/script.js
@@ -0,0 +1,13 @@
+// We use a for-loop to execute code more than once
+for (var i = 0; i < 5; i++) {
+    // This is the block of code that will run each time
+    console.log("This is the current value of i: " + i + ".");
+}
+
+// For-loops are often used to iterate over arrays
+var zooAnimals = ["monkeys", "penguins", "bats", "lions"]
+
+//To determine how many times the loop should execute, we use the array's length
+for (let i = 0; i < zooAnimals.length; i++) {
+    console.log(zooAnimals[i])
+}
